27e1249edabd9cd7b7e3aa866d64078d52fd6346
dederodhatulfarida <dede.118140059@student.itera.ac.id>
2021-05-07 04:25:45 +0700

    menambahkan banner slider pada home

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import { StyleSheet, Text, View , ImageBackground , TouchableOpacity , ScrollView} from 'react-native'
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  ImageBackground
+} from 'react-native';
 
-const Index = ({navigation}) => {
-    return (
-        <View style={styles.container}>
-            <ScrollView showsVerticalScrollIndicator={false}>
+const images = [
+  'https://i.postimg.cc/sMkyrXqM/taonga.png',
+  'https://i.postimg.cc/G24Jg2bk/banner2.jpg' ,
+  'https://i.postimg.cc/VsH2NrSc/Best-Types-of-Wood-for-Furniture.jpg'
+]
+
+const Kategori = ({navigation})=>{
+  return(
+  
+    <ScrollView showsVerticalScrollIndicator={false} style = {{marginTop:190, zIndex:-1}}>
+
+            <View style={styles.data}>
+                <TouchableOpacity style={styles.cover} onPress={() => navigation.navigate('Dinning')}>
+                    <ImageBackground style={styles.background} source={require('../../assets/img/dinningroom.jpg')} >
+                        <View style={styles.info}>
+                            <Text style={styles.text}>Ruang Makan</Text>
+                        </View>
+                    </ImageBackground>
+                </TouchableOpacity>
+            </View>
+            
             <View style={styles.data}>
                 <TouchableOpacity style={styles.cover} onPress={() => navigation.navigate('Bedroom')}>
                     <ImageBackground style={styles.background} source={require('../../assets/img/bedroom.jpg')} >
@@ -26,16 +53,6 @@ const Index = ({navigation}) => {
             </View>
 
             <View style={styles.data}>
-                <TouchableOpacity style={styles.cover} onPress={() => navigation.navigate('Dinning')}>
-                    <ImageBackground style={styles.background} source={require('../../assets/img/dinningroom.jpg')} >
-                        <View style={styles.info}>
-                            <Text style={styles.text}>Ruang Makan</Text>
-                        </View>
-                    </ImageBackground>
-                </TouchableOpacity>
-            </View>
-
-            <View style={styles.data}>
                 <TouchableOpacity style={styles.cover}>
                     <ImageBackground style={styles.background} source={require('../../assets/img/workingroom.jpg')} >
                         <View style={styles.info}>
@@ -44,16 +61,102 @@ const Index = ({navigation}) => {
                     </ImageBackground>
                 </TouchableOpacity>
             </View>
-            </ScrollView>
-        </View>
-    )
+
+     </ScrollView>
+
+  );
 }
 
-export default Index
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 0
+    }
+  }
+
+  change(nativeEvent) {
+    if(nativeEvent) {
+      const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
+      if (slide !== this.state.active) {
+        this.setState({
+          active:slide
+        })
+      }
+    }
+  }
+
+  render() {
+    const { active } = this.state;
+    return (
+      <View>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.wrap}>
+
+                    <ScrollView
+                    onScroll={({ nativeEvent })=>this.change(nativeEvent)}
+                    showsHorizontalScrollIndicator={false}
+                    pagingEnabled
+                    horizontal
+                    style={styles.wrap}
+                    >
+                    {
+                        images.map((e, index) =>
+                        <Image
+                            key={e}
+                            resizeMode="stretch"
+                            style={styles.wrap}
+                            source={{ uri: e }}
+                        />
+                        )
+                    }
+                    </ScrollView>
+
+                    <View style={styles.wrapDot}>
+                    {
+                        images.map((e, index) =>
+                        <Text
+                            key={e}
+                            style={active === index ? styles.dotActive : styles.dot}>●</Text>)
+                    }
+                    </View>
+
+                </View>
+        </SafeAreaView>
+
+        <Kategori/>
+              
+      </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
-    container : {
-        paddingTop : 30,
+  container: {
+    flex: 1,
+    paddingTop:20,
+  },
+  wrap: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height * 0.25 // 25% window
+  },
+  wrapDot: {
+    position: 'absolute',
+    bottom: 0,
+    flexDirection: 'row',
+    alignSelf: 'center'
+  },
+  dot: {
+    margin: 3,
+    color: '#888'
+  },
+  dotActive: {
+    margin: 3,
+    color: 'black'
+  },
+   container2 : {
+        paddingTop : 20,
         paddingHorizontal : 10 ,
         flex : 1 ,
         flexDirection : 'column' 
@@ -61,8 +164,8 @@ const styles = StyleSheet.create({
 
     data : {
         width : '100%' ,
-        height : 160 ,
-        marginBottom : 12
+        height : 187,
+        marginBottom : 5,
     } ,
 
     cover : {
@@ -79,7 +182,7 @@ const styles = StyleSheet.create({
 
     info : {
         position : 'absolute' ,
-        backgroundColor : '#C4C4C4' ,
+        backgroundColor : '#553BFB' ,
         padding : 10 ,
         borderTopLeftRadius : 8 ,
         bottom : 0 ,
@@ -93,3 +196,5 @@ const styles = StyleSheet.create({
     }
 
 })
+
+export default Index;
