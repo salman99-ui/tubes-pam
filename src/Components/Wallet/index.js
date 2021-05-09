f670dd0cd1be5c3f0ef52299a627334b54a25663
Salman damanhuri <slmndamanhuri@gmail.com>
2021-05-09 22:51:33 +0700

    fixing apps

--- src/Components/Wallet/index.js
+++ src/Components/Wallet/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image, Pressable, ScrollView } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import {useSelector} from 'react-redux'
+import {DataTable} from 'react-native-paper'
 const Wallet=() => {
   const data = useSelector(state => state.wallet.last_transaction)
   const saldo = useSelector(state => state.wallet.saldo)
-  const test = JSON.stringify(data)
+  
   return (
     <View style={{flex:1,  paddingHorizontal : 8}}> 
       <View style={{backgroundColor:'white', height:267, alignItems:'center', borderBottomWidth:2, borderBottomColor:'#B1B1B1'}}>
@@ -17,14 +18,26 @@ const Wallet=() => {
        
         <View style={{ backgroundColor:'white', marginHorizontal:5 }}/>
           <ScrollView style={{ backgroundColor:'white', paddingTop:27}}>
+            <DataTable>
+              <DataTable.Header>
+                <DataTable.Title>Date</DataTable.Title>
+                <DataTable.Title>Items</DataTable.Title>
+                <DataTable.Title>Price</DataTable.Title>
+              </DataTable.Header>
+
+            
             {
               data.map((items , index) =>(
-                <View key={index} style={{marginBottom : 15}}>
-                <Text  style={{fontWeight : '700' , fontSize : 14 , color : '#2E42AA'}}>Buy {items.name} Price ${items.price}</Text>
-                <View style={{ backgroundColor:'white', width : '10%' , borderBottomWidth : 2 ,marginTop : 4}}/>
-                </View>
+                <DataTable.Row key={index}>
+                  <DataTable.Cell>{items.calender}</DataTable.Cell>
+                  <DataTable.Cell>{items.name}</DataTable.Cell>
+                  <DataTable.Cell>${items.price}</DataTable.Cell>
+                  
+                </DataTable.Row>
+                
               ) )
             }
+            </DataTable>
           </ScrollView>
       </View>
     </View>
